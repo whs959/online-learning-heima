0ebd7452cb49691b2535cd2260e51adc1c71e54d
wanghansheng <1761399570@qq.com>
2024-06-11 08:43:03 +0800

    【媒资模块】1.测试分支合并 【王汉升】

---
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/test/com/xuecheng/media/Test.java
@@ -0,0 +1,13 @@
+package com.xuecheng.media;
+
+/**
+ * Author: whs
+ * Date: 2024/6/11 8:42
+ * FileName: Test
+ * Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试合并");
+    }
+}
